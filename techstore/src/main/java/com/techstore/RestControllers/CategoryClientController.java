ecc8e8c4a45320b7827e38c8519aef2f0adb8d95
DESKTOP\Administrator <huyducphamofficial@gmail.com>
2021-04-01 21:02:14 +0700

    Fix: Change ProductClientObject propery names Feat: Add category client controller

--- techstore/src/main/java/com/techstore/RestControllers/CategoryClientController.java
+++ techstore/src/main/java/com/techstore/RestControllers/CategoryClientController.java
@@ -5,10 +5,38 @@
  */
 package com.techstore.RestControllers;
 
+import com.techstore.repositories.client.CategoryClientRepository;
+import com.techstore.services.CategoryService;
+import com.techstore.techstore.apiobject.CategoryClientObject;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  *
  * @author Administrator
  */
+@Controller
+@RequestMapping("/client")
 public class CategoryClientController {
-    
+
+    @Autowired
+    private CategoryClientRepository categoryClientRepository;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/getloaisp")
+    public @ResponseBody
+    List<CategoryClientObject> getSpAll() {
+        if (categoryClientRepository.all().isEmpty()) {
+            categoryService.all().forEach(productEntity -> {
+                categoryClientRepository.add(new CategoryClientObject(productEntity));
+            });
+        }
+        return categoryClientRepository.all();
+    }
 }
